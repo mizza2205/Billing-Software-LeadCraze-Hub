9858cb537a40f94f34348acb4aa00f483dd0cace
misbah <you@example.com>
2026-03-23 14:39:16 +0530

    added some files in backend

--- Backend-block/server.js
+++ Backend-block/server.js
@@ -1,24 +1,80 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const bcrypt = require('bcrypt');
+const db = require('./db');
+const jwt = require('jsonwebtoken');
 
 const app = express();
-
-// middleware
-app.use(cors());
 app.use(express.json());
 
 // test route
-app.get("/", (req, res) => {
-  res.send("Billing Software Backend Running 🚀");
+app.get('/', (req, res) => {
+  res.send('Server running ');
 });
 
-// example API
-app.get("/api/test", (req, res) => {
-  res.json({ message: "API working successfully" });
+// DB test
+app.get('/test-db', (req, res) => {
+  db.query('SELECT 1', (err, result) => {
+    if (err) return res.send(err);
+    res.send('DB Connected done here');
+  });
 });
 
-// server start
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  const sql = 'SELECT * FROM users WHERE email = ?';
+
+  db.query(sql, [email], async (err, result) => {
+    if (err) return res.send(err);
+
+    if (result.length === 0) {
+      return res.send('User not found ');
+    }
+
+    const user = result[0];
+
+    // password compare
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.send('Wrong password ');
+    }
+
+    // JWT token generate
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      'secretkey',
+      { expiresIn: '1h' }
+    );
+
+    res.json({
+      message: 'Login successful ',
+      token: token
+    });
+  });
+});
+
+
+app.post('/signup', async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    // password hash
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const sql = 'INSERT INTO users (name, email, password) VALUES (?, ?, ?)';
+
+    db.query(sql, [name, email, hashedPassword], (err, result) => {
+      if (err) return res.send(err);
+
+      res.send('User created (secure) ');
+    });
+
+  } catch (err) {
+    res.send(err);
+  }
+});
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
 });
